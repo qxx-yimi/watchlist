e72277a151d504afc09cb41f660315f361f1a615
meter <1462068546@qq.com>
2024-01-31 14:16:24 +0800

    add database support with flask-sqlalchemy

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
 from flask import Flask, url_for, render_template
 from markupsafe import escape
+from flask_sqlalchemy import SQLAlchemy
+import os
+import click
 
 app = Flask(__name__)
+# 先加载配置
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+db = SQLAlchemy(app)
+
+
+# database model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
 
 
 @app.route('/user/<name>')
@@ -27,16 +48,35 @@ def test_url_for():
     return 'Test page'
 
 
-name = 'one meter'
-movies = [
-    {'title': '我的邻居托托罗', 'year': '1988'},
-    {'title': '死亡诗社', 'year': '1989'},
-    {'title': '完美世界', 'year': '1993'},
-    {'title': '这个杀手不太冷', 'year': '1994'},
-    {'title': '麻将', 'year': '1996'},
-    {'title': '燕尾蝶', 'year': '1996'},
-    {'title': '喜剧之王', 'year': '1999'},
-    {'title': '鬼子来了', 'year': '1999'},
-    {'title': '机器人总动员', 'year': '2008'},
-    {'title': '音乐之猪', 'year': '2012'},
-]
+@app.cli.command() # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after app.')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo("Initialized database.")
+
+
+@app.cli.command()
+def forge():
+    db.create_all()
+    name = 'one meter'
+    movies = [
+        {'title': '我的邻居托托罗', 'year': '1988'},
+        {'title': '死亡诗社', 'year': '1989'},
+        {'title': '完美世界', 'year': '1993'},
+        {'title': '这个杀手不太冷', 'year': '1994'},
+        {'title': '麻将', 'year': '1996'},
+        {'title': '燕尾蝶', 'year': '1996'},
+        {'title': '喜剧之王', 'year': '1999'},
+        {'title': '鬼子来了', 'year': '1999'},
+        {'title': '机器人总动员', 'year': '2008'},
+        {'title': '音乐之猪', 'year': '2012'},
+    ]
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done.')
