9d5936151e19ce377d0f75d50a45908e3c716f8a
meter <1462068546@qq.com>
2024-02-03 10:05:17 +0800

    Ready to deploy

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+# 手动设置环境变量并导入程序实例
+import os
+
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+from watchlist import app
